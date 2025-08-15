199b7b0092f683fc2a6762da80850ebb1aaf3e7a
yunjian <yunjian.lu@gmail.com>
2025-08-15 15:20:25 -0700

    path

--- lightyear/next.config.mjs
+++ lightyear/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+const isDev = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
-  basePath: "/lightyear",
+  basePath: isDev ? "" : "/lightyear",
 };
+
 export default nextConfig;
